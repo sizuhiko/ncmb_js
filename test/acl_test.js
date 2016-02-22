86f54cc3c1a6b40dda7d0619ef35cc756c465c04
fuku2015 <k.fukumoto49@gmail.com>
2016-02-22 13:46:47 +0900

    fixed tests to use stub field

--- test/acl_test.js
+++ test/acl_test.js
@@ -12,7 +12,8 @@ describe("NCMB ACL", function(){
       .set("protocol", config.apiserver.protocol || "http:")
       .set("fqdn", config.apiserver.fqdn)
       .set("port", config.apiserver.port)
-      .set("proxy", config.apiserver.port || "");
+      .set("proxy", config.apiserver.port || "")
+      .set("stub", config.apiserver.stub);
   }
 
   var aclObj = null;
